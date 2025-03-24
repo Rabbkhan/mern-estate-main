3cc857fc8d1a419128a6e0f4cfec35a27191368b
rabbkhan <rabbilkhan125@gmail.com>
2025-03-25 04:47:05 +0530

    about section changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,10 @@
+import lineClamp from '@tailwindcss/line-clamp';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-    // ...
-  ],
+  plugins: [lineClamp],
 };
